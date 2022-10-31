c873be3e632549c82e6ce7a8ec0c1d4cdd25cbb3
Ana Maria Arbelaez L <anam.arbelaezlopez@gmail.com>
2022-10-30 23:43:47 -0500

    added stimulus controller

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,3 +6,6 @@ import { application } from "./application"
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+import TreeController from "./tree_controller"
+application.register("tree", TreeController)
